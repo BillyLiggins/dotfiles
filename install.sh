c31c022be6c3521f6dbde992a5297390eec4abcf
billyliggins-yobota <billy@yobota.xyz>
2019-10-28 09:28:06 +0000

    make nvim directory.

--- install.sh
+++ install.sh
@@ -3,14 +3,16 @@ DIR=${PWD}
 
 for file in $DIR/*; do
     filename=${file##*/}
-    if [ "$filename" == "install.sh" ] || [ "$filename" == "README.md" ] ; then
+    if [ "$filename" == "init.vim" ] || [ "$filename" == "install.sh" ] || [ "$filename" == "README.md" ] ; then
         continue
     fi
     echo "Creating symlink to $file"
     ln -sfn $file ~/.$filename
 done
 
+mkdir -p ~/.config/nvim/init.vim
+ln -sfn init.vim ~/.config/nvim/init.vim
+
 source ~/.bashrc
 source ~/.bash_profile
 echo -e "\nDONE!"
-echo "YouCompleteMe link: https://github.com/Valloric/YouCompleteMe"
